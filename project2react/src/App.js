ca35b0e0e785d26f235d2556b8e5bff1b0a7b7a9
Justin <jgrove2@travelers.com>
2023-07-05 15:53:14 -0400

    film and cors

--- project2react/src/App.js
+++ project2react/src/App.js
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router-dom';
 import './App.css';
 import Home from './components/Home.js'
 import Character from './components/Character';
+import Film from './components/Film';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='character/:id' element={<Character />} />
+        <Route path='films/:id' element={<Film />} />
       </Routes>
     </div>
   );
