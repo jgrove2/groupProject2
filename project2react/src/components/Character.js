bd9f25274380f78907ffbc83154c325d277ec16d
Lena <you@example.com>
2023-07-05 22:07:02 -0400

    character for homeworld

--- project2react/src/components/Character.js
+++ project2react/src/components/Character.js
@@ -1,29 +1,58 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
 const Character = (props) => {
-    const {id} = useParams()
+  const { id } = useParams();
 
-    const [character,setCharacter] = useState({name:undefined, mass:undefined, birth_year:undefined})
-    const findCharacter = async () => {
-        const details = await fetch(`http://localhost:3001/api/characters/${id}`)
-        const detailsjson = await details.json()
-        console.log(detailsjson)
-        setCharacter(detailsjson[0])
-    }
+  const [character, setCharacter] = useState({
+    name: "",
+    mass: "",
+    birth_year: "",
+  });
+  const [homeworldId, setHomeWorldId] = useState("");
+  const [homeworldName, setHomeworldName] = useState("");
 
-    useEffect(() => {findCharacter()
-    }, [])
-    return(
-            <>
-            <h1>{character.name}</h1>
-            <p>{character.height}</p>
-            <p>{character.mass}</p>
-            <p>{character.birth_year}</p>
-            </>
-    )
+  const findCharacter = async () => {
+    const characterResponse = await fetch(
+      `http://localhost:3001/api/characters/${id}`
+    );
 
-    
-}
+    const characterObj = await characterResponse.json();
+
+    const tempCharacter = characterObj[0];
+
+    console.log(tempCharacter);
+    setCharacter(tempCharacter);
+    setHomeWorldId(tempCharacter?.homeworld);
+  };
+
+  const findHomeworld = async () => {
+    const homeworldResponse = await fetch(
+      `http://localhost:3001/api/planets/${homeworldId}`
+    );
+    const homeworldObj = await homeworldResponse.json();
+    const tempHomeworld = homeworldObj[0];
+    console.log(tempHomeworld.name);
+
+    setHomeworldName(tempHomeworld?.name);
+  };
+
+  useEffect(() => {
+    findCharacter();
+    findHomeworld();
+  }, []);
+  return (
+    <>
+      <h1>{character.name}</h1>
+      <p>{character.height}</p>
+      <p>{character.mass}</p>
+      <p>{character.birth_year}</p>
+      <div>
+        <h1>Homeworld</h1>
+        <p>{homeworldName}</p>
+      </div>
+    </>
+  );
+};
 
 export default Character;
--- project2react/src/components/HomeCharacter.js
+++ project2react/src/components/HomeCharacter.js
@@ -1,18 +1,14 @@
 import React from "react";
-import {Link} from 'react-router-dom';
-import Character from "./Character";
+import { Link } from "react-router-dom";
 
-const HomeCharacter = ({...props}) => {
-    return (
-        <>
-            <div>
-                <Link to={`/character/${props.id}`}>
-                    {props.name}
-                    <Character/>
-                </Link>
-            </div>
-        </>
-    )
-}
+const HomeCharacter = ({ ...props }) => {
+  return (
+    <>
+      <div>
+        <Link to={`/character/${props.id}`}>{props.name}</Link>
+      </div>
+    </>
+  );
+};
 
 export default HomeCharacter;
