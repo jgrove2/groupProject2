28e0bb59158a8f14ad21ca389f97f8f94dbffc67
Lena <you@example.com>
2023-07-06 09:47:58 -0400

    homeworld

--- project2react/src/components/Character.js
+++ project2react/src/components/Character.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 const Character = (props) => {
   const { id } = useParams();
@@ -10,7 +10,8 @@ const Character = (props) => {
     birth_year: "",
   });
   const [homeworldId, setHomeWorldId] = useState("");
-  const [homeworldName, setHomeworldName] = useState("");
+  const [homeworldName, setHomeworldName] = useState([]);
+  const [films, setFilm] = useState([]);
 
   const findCharacter = async () => {
     const characterResponse = await fetch(
@@ -21,9 +22,18 @@ const Character = (props) => {
 
     const tempCharacter = characterObj[0];
 
+    const homeworldResponse = await fetch(
+      `http://localhost:3001/api/planets/${tempCharacter.id}`
+    );
+    const homeworldObj = await homeworldResponse.json();
+    const tempHomeworld = homeworldObj;
+
     console.log(tempCharacter);
     setCharacter(tempCharacter);
     setHomeWorldId(tempCharacter?.homeworld);
+    // findHomeworld();
+    setHomeworldName(tempHomeworld);
+    console.log(tempHomeworld);
   };
 
   const findHomeworld = async () => {
@@ -31,25 +41,59 @@ const Character = (props) => {
       `http://localhost:3001/api/planets/${homeworldId}`
     );
     const homeworldObj = await homeworldResponse.json();
-    const tempHomeworld = homeworldObj[0];
-    console.log(tempHomeworld.name);
+    const tempHomeworld = homeworldObj;
+    console.log(homeworldId);
+    console.log(tempHomeworld[parseInt(homeworldId)]);
 
-    setHomeworldName(tempHomeworld?.name);
+    setHomeworldName(tempHomeworld);
+  };
+
+  const findFilm = async () => {
+    const filmResponse = await fetch(
+      `http://localhost:3001/api/characters/${id}/films`
+    );
+    const filmObj = await filmResponse.json();
+    let filmDetails = [];
+    console.log(filmObj);
+    for await (let film of filmObj) {
+      const filmResponse1 = await fetch(
+        `http://localhost:3001/api/films/${film.film_id}`
+      );
+      const filmJson = await filmResponse1.json();
+      filmDetails.push(filmJson[0]);
+    }
+    setFilm(filmDetails);
   };
 
   useEffect(() => {
     findCharacter();
-    findHomeworld();
+    findFilm();
   }, []);
   return (
     <>
       <h1>{character.name}</h1>
-      <p>{character.height}</p>
-      <p>{character.mass}</p>
-      <p>{character.birth_year}</p>
+      <span>{character.height}</span>
+      <span>{character.mass}</span>
+      <span>{character.birth_year}</span>
       <div>
         <h1>Homeworld</h1>
-        <p>{homeworldName}</p>
+        {homeworldName.map((h, index) => {
+          return (
+            <span key={index}>
+              <Link to={`/planets/${h.id}`}>{h.name}</Link>
+            </span>
+          );
+        })}
+      </div>
+      <div>
+        <h1>Film</h1>
+        {films.map((f, index) => {
+          return (
+            <span key={index}>
+              <Link to={`/films/${f.id}`}>{f.title}</Link>
+            </span>
+          );
+        })}
       </div>
     </>
   );
